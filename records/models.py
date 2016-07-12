6143472732fdf3fd30cf6e603fc91f8106bddf66
Hany Kasem <hanykasem@hotmail.com>
2016-07-12 23:13:32 +0400

    Added a basic details html and linked visits to patient records

--- records/models.py
+++ records/models.py
@@ -8,8 +8,15 @@ class Patient(models.Model):
     presentation_date = models.DateField()
     presentation = models.TextField()
 
+    def __str__(self):
+        return self.name
+
+
 class Visit(models.Model):
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
     visit_date = models.DateField()
     visit_note = models.TextField()
     visit_image = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return str(self.visit_date)
--- records/views.py
+++ records/views.py
@@ -1,14 +1,14 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.http import Http404
+from django.shortcuts import render
 from .models import Patient
 
 def index(request):
     all_patients = Patient.objects.all()
-    template = loader.get_template('records/index.html')
-    context = {
-        'all_patients': all_patients,
-    }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'records/index.html', {'all_patients': all_patients})
 
 def detail(request, record_id):
-    return HttpResponse("<h2>Record detail page for ID number: " + str(record_id) + "</h2>")
+    try:
+        patient = Patient.objects.get(pk=record_id)
+    except Patient.DoesNotExist:
+        raise Http404("Record does not exist")
+    return render(request, 'records/detail.html', {'patient': patient})
