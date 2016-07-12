bbd43493b4c43d54d78cf1b9034828a0d435098b
Hany Kasem <hanykasem@hotmail.com>
2016-07-12 21:24:45 +0400

    Added a basic HTML template

--- records/views.py
+++ records/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
+from django.template import loader
+from .models import Patient
 
 def index(request):
-    return HttpResponse("<h1>Welcome to the OMFS Case Records Platform!</h1>")
+    all_patients = Patient.objects.all()
+    template = loader.get_template('records/index.html')
+    context = {
+        'all_patients': all_patients,
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, record_id):
     return HttpResponse("<h2>Record detail page for ID number: " + str(record_id) + "</h2>")
