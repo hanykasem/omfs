cb4129ce6b21d27ecc16eb9a86ae221acb4bc8d6
Hany Kasem <hanykasem@hotmail.com>
2016-07-12 20:29:47 +0400

    Added a details basic view

--- records/views.py
+++ records/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1>Welcome to the OMFS Case Records Platform!</h1>")
+
+def detail(request, record_id):
+    return HttpResponse("<h2>Record detail page for ID number: " + str(record_id) + "</h2>")
